6876c001a7e5126c1cbd193c0e72b25a6dbe5eae
cfletcher-qa <carmenkfletcher@gmail.com>
2025-12-11 14:14:57 +0000

    add inventory page and tests

---
+++ ui-automation/pages/inventory.page.ts
@@ -0,0 +1,45 @@
+import { Page, Locator } from '@playwright/test';
+
+export class InventoryPage {
+readonly page: Page;
+readonly pageTitle: Locator;
+readonly cartLink: Locator;
+readonly cartBadge: Locator;
+
+
+constructor(page: Page){
+this.page = page;
+this.pageTitle = page.locator('span.title');
+this.cartLink = page.locator('.shopping_cart_link');
+this.cartBadge = page.locator('.shopping_cart_badge');
+}
+
+async getTitle(){
+return this.pageTitle.textContent();
+}
+
+async addItemToCart(itemName: string){
+    const selector = '[data-test="add-to-cart-${this.formatItemName(itemName)}"]';
+    await this.page.locator(selector).click();
+}
+
+async removeItemCart(itemName: string){
+    const selector = '[data-test="remove-${this.formatItemName(itemName)}"]';
+    await this.page.locator(selector).click();
+}
+
+async openCart() {
+ await this.cartLink.click();
+}
+
+ async getCartCount(): Promise<number> {
+    const text = await this.cartBadge.textContent();
+    return text ? parseInt(text) : 0;
+}
+
+  private formatItemName(itemName: string): string {
+    return itemName.toLowerCase().replace(/ /g, '-');
+}
+
+}
+
---
+++ ui-automation/tests/inventory.spec.ts
@@ -0,0 +1,4 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { InventoryPage } from '../pages/inventory.page';
+
